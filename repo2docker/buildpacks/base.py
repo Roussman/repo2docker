74555d86f13967861f8d6207e49590ba03811ea6
Roussman <150775061+Roussman@users.noreply.github.com>
2025-03-14 15:19:08 +0100

    Update base.py

--- repo2docker/buildpacks/base.py
+++ repo2docker/buildpacks/base.py
@@ -44,15 +44,15 @@ ENV USER=${NB_USER} \
     HOME=/home/${NB_USER}
 
 RUN groupadd \
-        --gid 8305 \
+        --gid ${NB_UID}  \
         ${NB_USER} && \
     useradd \
         --comment "Default user" \
         --create-home \
-        --gid 8305 \
+        --gid ${NB_UID}  \
         --no-log-init \
         --shell /bin/bash \
-        --uid 123653 \
+        --uid ${NB_UID}  \
         ${NB_USER}
 
 # Base package installs are not super interesting to users, so hide their outputs
